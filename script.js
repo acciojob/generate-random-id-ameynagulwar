3b22a89fd3fd2b5494ef67cc3f9aee4503042e8c
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-14 17:41:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let newString = "";
-  for(let i = 0; i <= l; i++){
-    newString += 'A'
+function makeid(l){
+	const string = 'abcdefghijklmnopqrstuvwxyzzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let resultString = '';
+  for(let i = 0; i < l; i++){
+    const randomIndex = Math.floor(Math.random() * string.length);
+    resultString += string[randomIndex];
   }
-	return newString;
+  return resultString;
 }
 
 // Do not change the code below.
